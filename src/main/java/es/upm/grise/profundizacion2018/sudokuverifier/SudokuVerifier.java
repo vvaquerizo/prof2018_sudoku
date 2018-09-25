d6f4f8ddd1506ba9b82c0c9c249e23a0169e503d
Oscar Dieste <odieste@fi.upm.es>
2018-09-25 18:38:15 +0200

    Project stub

---
+++ src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
@@ -0,0 +1,9 @@
+package es.upm.grise.profundizacion2018.sudokuverifier;
+
+public class SudokuVerifier 
+{
+	public int verify (String candidateSolution)
+    {
+        return 0;
+    }
+}
---
+++ src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
@@ -0,0 +1,13 @@
+package es.upm.grise.profundizacion2018.sudokuverifier;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class SudokuVerifierTest {
+    
+	@Test
+	public void test() {
+    		fail("Not yet implemented");
+    }
+}
