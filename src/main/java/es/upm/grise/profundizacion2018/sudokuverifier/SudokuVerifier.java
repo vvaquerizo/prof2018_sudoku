be945b6a0ec814997d47a480cf9df2975e8fc232
vvaquerizo <vvaquerizo@alumnos.upm.es>
2018-10-03 00:02:20 +0200

    Implementación de Sudoku Verifier

--- src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
+++ src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
@@ -2,8 +2,130 @@ package es.upm.grise.profundizacion2018.sudokuverifier;
 
 public class SudokuVerifier 
 {
+	private final int SUDOKU_SIDE_SIZE = 9;
+	private final int SUBGRID_SIDE_SIZE = 3;
+	
+	private String sudokuString;
+	private String[] sudokuStringArray;
+	private int [][] sudoku = new int [9][9];
+	
 	public int verify (String candidateSolution)
     {
+		this.sudokuString = candidateSolution;
+		
+		string2stringArray();
+		
+		if (!isValidString()) {
+			return -1;
+		}
+		
+		string2matrix();
+		
+		if (!areValidSubgrids()) {
+			return -2;
+		}
+		
+		if (!areValidRows()) {
+			return -3;
+		}
+		
+		if (!areValidColumns()) {
+			return -4;
+		}
+		
         return 0;
     }
+	
+	private void string2stringArray() {
+		sudokuStringArray = sudokuString.split("");
+	}
+	
+	private boolean isValidString() {
+		if (sudokuStringArray.length != 81) {
+			return false;
+		}
+		
+		for (int i = 0; i < sudokuStringArray.length; i++) {
+			if (!(sudokuStringArray[i].matches("^[1-9]$"))) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private void string2matrix() {
+		for (int row = 0; row < SUDOKU_SIDE_SIZE; row++) {
+			for (int column = 0; column < SUDOKU_SIDE_SIZE; column++) {
+				sudoku[row][column] = Integer.parseInt(sudokuStringArray[row*SUDOKU_SIDE_SIZE + column]);
+			}
+		}
+	}
+	
+	private boolean areValidSubgrids() {
+		for (int startRow = 0; startRow < SUDOKU_SIDE_SIZE; startRow = startRow + SUBGRID_SIDE_SIZE) {
+			for (int startColumn = 0; startColumn < SUDOKU_SIDE_SIZE; startColumn = startColumn + SUBGRID_SIDE_SIZE) {
+				if (hasDuplicatedElements(obtainSubgridArray(startRow,startColumn))) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	private Integer[] obtainSubgridArray(int startRow, int startColumn) {
+		Integer[] subgridStringArray = new Integer[9];
+		for (int row = startRow; row < (startRow + SUBGRID_SIDE_SIZE); row++) {
+			for (int column = startColumn; column < (startColumn + SUBGRID_SIDE_SIZE); column++) {
+				subgridStringArray[(row - startRow)*SUBGRID_SIDE_SIZE + (column - startColumn)] = sudoku[row][column];
+			}
+		}
+		return subgridStringArray;
+	}
+	
+	private boolean hasDuplicatedElements(Integer[] integerArray) {
+		for (int i = 0; i < integerArray.length; i++) {
+			for (int j = 0; j < integerArray.length; j++) {
+				if (i != j) {
+					if (integerArray[i].equals(integerArray[j])) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	private boolean areValidRows() {
+		for (int row = 0; row < SUDOKU_SIDE_SIZE; row++) {
+			if (hasDuplicatedElements(obtainRow(row))) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private Integer[] obtainRow(int row) {
+		Integer[] rowArray = new Integer[SUDOKU_SIDE_SIZE];
+		for (int i = 0; i < SUDOKU_SIDE_SIZE; i++) {
+			rowArray[i] = sudoku[row][i];
+		}
+		return rowArray;
+	}
+	
+	private boolean areValidColumns() {
+		for (int column = 0; column < SUDOKU_SIDE_SIZE; column++) {
+			if (hasDuplicatedElements(obtainColumn(column))) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private Integer[] obtainColumn(int column) {
+		Integer[] columnArray = new Integer[SUDOKU_SIDE_SIZE];
+		for (int i = 0; i < SUDOKU_SIDE_SIZE; i++) {
+			columnArray[i] = sudoku[i][column];
+		}
+		return columnArray;
+	}
 }
--- src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
@@ -1,13 +1,57 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class SudokuVerifierTest {
     
+	//Invalid string length: 79 characters
 	@Test
-	public void test() {
-    		fail("Not yet implemented");
+	public void invalidStringLength() {
+    	SudokuVerifier sudokuVerifier = new SudokuVerifier();
+		assertEquals(-1, sudokuVerifier.verify("5555555555555555555555555555555555555555555555555555555555555555555555555555555"));
     }
+	
+	//Invalid string format: contains a non-positive digit (0)
+	@Test
+	public void invalidStringDigit() {
+    	SudokuVerifier sudokuVerifier = new SudokuVerifier();
+		assertEquals(-1, sudokuVerifier.verify("055555555555555555555555555555555555555555555555555555555555555555555555555555555"));
+    }
+	
+	//Invalid string format: contains letter (B)
+		@Test
+		public void invalidStringFormat() {
+	    	SudokuVerifier sudokuVerifier = new SudokuVerifier();
+			assertEquals(-1, sudokuVerifier.verify("B55555555555555555555555555555555555555555555555555555555555555555555555555555555"));
+	    }
+	
+	//Invalid subgrid
+	@Test
+	public void invalidSubgrid() {
+    	SudokuVerifier sudokuVerifier = new SudokuVerifier();
+		assertEquals(-2, sudokuVerifier.verify("278539164524216987916874352783452691149687523625193748452761839897325416361948275"));
+    }
+	
+	//Invalid row
+	@Test
+	public void invalidRow() {
+    	SudokuVerifier sudokuVerifier = new SudokuVerifier();
+		assertEquals(-3, sudokuVerifier.verify("532678912674195348198342567859761423426853791713924856961537284287419635345286179"));
+    }
+	
+	//Invalid column
+	@Test
+	public void invalidColumn() {
+	    SudokuVerifier sudokuVerifier = new SudokuVerifier();
+		assertEquals(-4, sudokuVerifier.verify("534678912627195348198342567859761423426853791713924856961537284287419635345286179"));
+	}
+	
+	//Valid sudoku string
+	@Test
+	public void validSudoku() {
+	    SudokuVerifier sudokuVerifier = new SudokuVerifier();
+		assertEquals(0, sudokuVerifier.verify("524163897791854263683729154412976385857341926369582741976235418248617539135498672"));
+	}
 }
